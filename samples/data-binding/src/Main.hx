826ae60280b38ea760677f491f7216643460ef4c
Josh Tynjala <joshtynjala@bowlerhat.dev>
2023-08-29 14:00:13 -0700

    new sample: data-binding

---
+++ samples/data-binding/src/Main.hx
@@ -0,0 +1,9 @@
+import minimalcomps.components.mxhx.Application;
+import openfl.events.MouseEvent;
+
+@:build(mxhx.macros.MXHXComponent.build())
+class Main extends Application {
+	public function new() {
+		super();
+	}
+}
